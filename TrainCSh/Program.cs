d8a283c277504680fc641ac466a2f3ccb6e30897
Vadim <kalugavadim04@gmail.com>
2021-10-26 22:11:38 +0700

    Добавил TrainRoute

--- TrainCSh/Program.cs
+++ TrainCSh/Program.cs
@@ -12,6 +12,15 @@ namespace TrainCSh
             passenger.UserInput();
             vadim.Print();
             passenger.Print();
+
+            DateTime day1 = new DateTime(2021, 10, 26, 21, 59, 0);
+            DateTime day2 = new DateTime(2021, 10, 30, 12, 17, 0);
+            TrainRoute route = new TrainRoute("Барнаул", "Омск", day1, day2);
+            route.Print();
+
+
+
+
         }
     }
 }
---
+++ TrainCSh/TrainRoute.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace TrainCSh
+{
+    class TrainRoute
+    {
+        private string departure;
+        private string destination;
+        private DateTime departureDate;
+        private DateTime destinationDate;
+        private string dateFormat = "dd.MM.yyyy HH:mm";
+
+
+        public TrainRoute() : this("-", "-", new DateTime(), new DateTime()) { }
+
+        public TrainRoute(string departure, string destination, DateTime departureDate, DateTime destinationDate)
+        {
+            if (IsValidDeparture(departure) && IsValidDestination(destination) && departureDate != null && destinationDate != null)
+            {
+                this.departure = departure;
+                this.destination = destination;
+                this.departureDate = departureDate;
+                this.destinationDate = destinationDate;
+            } else
+            {
+                this.departure = "-";
+                this.destination = "-";
+                this.departureDate = new DateTime();
+                this.destinationDate = new DateTime();
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Дата отправления: {0}", departureDate.ToString(dateFormat));
+            Console.WriteLine("Дата прибытия: {0}", destinationDate.ToString(dateFormat));
+            TimeSpan travel = TravelTime();
+            Console.WriteLine("Время в пути: {0} дней, {1} часов, {2} минут", travel.Days, travel.Hours, travel.Minutes);
+        }
+
+        private TimeSpan TravelTime()
+        {
+            TimeSpan travel = destinationDate.Subtract(departureDate);
+            return travel;
+        }
+
+        public string Departure 
+        {
+            get
+            {
+                return departure;
+            }
+
+            set
+            {
+                if (IsValidDeparture(value))
+                {
+                    departure = value;
+                }
+            }
+        }
+
+        public string Destination 
+        {
+            get
+            {
+                return destination;
+            }
+
+            set
+            {
+                if (IsValidDestination(value))
+                {
+                    destination = value;
+                }
+            } 
+        }
+
+        public DateTime DepartureDate 
+        {
+            get
+            {
+                return departureDate;
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    departureDate = value;
+                }
+
+            } 
+        }
+
+        public DateTime DestinationDate 
+        {
+            get
+            {
+                return destinationDate;
+            }
+
+            set
+            {
+                if (destinationDate != null)
+                {
+                    destinationDate = value;
+                }
+            } 
+        }
+
+        private bool IsValidDeparture(string departure)
+        {
+            return departure.Trim().Length > 0;
+        }
+
+        private bool IsValidDestination(string destination)
+        {
+            return destination.Trim().Length > 0;
+        }
+    }
+}
