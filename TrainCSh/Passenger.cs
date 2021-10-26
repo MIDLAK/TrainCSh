3847504368702cfce4478f046f11229301a09895
Vadim <kalugavadim04@gmail.com>
2021-10-26 19:45:56 +0700

    Добавьте файлы проекта.

---
+++ TrainCSh/Passenger.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainCSh
+{
+    class Passenger
+    {
+        private string name;
+        private string firstName;
+        private int age;
+
+        public Passenger() : this("-", "-", 1) { }
+
+        public Passenger(string name, string firstName, int age)
+        {
+            if (IsValidAge(age) && IsValidName(name) && IsValidFirstName(firstName))
+            {
+                this.Name = name;
+                this.FirstName = firstName;
+                this.Age = age;
+            } else
+            {
+                this.Name = "-";
+                this.FirstName = "-";
+                this.Age = 1;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                if (IsValidName(value))
+                {
+                    name = value;
+                }
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+
+            set
+            {
+                if (IsValidFirstName(value))
+                {
+                    firstName = value;
+                }
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                if (IsValidAge(value))
+                {
+                    age = value;
+                }
+            }
+
+        }
+
+        private bool IsValidAge(int age)
+        {
+            return age > 0;
+        }
+
+        private bool IsValidName(string name)
+        {
+            return name.Trim().Length > 0;
+        }
+
+        private bool IsValidFirstName(string firstName)
+        {
+            return firstName.Trim().Length > 0;
+        }
+
+    }
+}
---
+++ TrainCSh/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TrainCSh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Passenger vadim = new Passenger("Вадим", "Калга", 19);
+            vadim.FirstName = "Калуга";
+            Passenger passenger = new Passenger();
+            Console.WriteLine("Первый пассажир: {1} {0} и ему {2} лет", vadim.Name, vadim.FirstName, vadim.Age);
+        }
+    }
+}
